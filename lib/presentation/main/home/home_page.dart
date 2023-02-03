a4dda3bce3451185c7f5159361c9ba16b7f8194f
Mazharul Islam Tohin <89094831+toh1n@users.noreply.github.com>
2023-02-04 00:28:09 +0600

    Add files via upload

--- lib/presentation/main/home/home_page.dart
+++ lib/presentation/main/home/home_page.dart
@@ -12,6 +12,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return const Center(
+
       child: Text(AppStrings.home),
     );
   }
--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -14,7 +14,7 @@ class AppStrings {
   static const String usernameError = "Username is wrong";
   static const String passwordError = "password is wrong";
   static const String password = "Password";
-  static const String forgetPassword = "Forget password";
+  static const String forgetPassword = "Forgot password?";
   static const String registerText1 = "Not a member ? ";
   static const String registerText2 = "Register Now!";
   static const String welcome = "Welcome back!";
